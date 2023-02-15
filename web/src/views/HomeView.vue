76d44f597986c67762550642e15d671632a8ed8e
rtof <zuichuan@msn.com>
2023-02-15 11:01:31 -0300

    init project

---
+++ web/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
