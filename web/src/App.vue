d63fb090a2e7f15fbaed89e9558a92f91bb957c3
rtof83 <zuichuan@msn.com>
2023-02-16 16:07:10 -0300

    adding sass

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Minhas Tarefas</router-link> |
+    <router-link to="/about">Configurações</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
+  @import '@/assets/css/global.css';
+
 /* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,5 +1,50 @@
 <template>
-  
+
+  <div class="container-home">
+    <div class="container-home__panel">
+      <h1>Minhas Tarefas</h1>
+
+      <!-- div input + botao -->
+      <div class="container-home__task">
+        <input type="text">
+        <button>adicionar</button>
+      </div>
+
+      <!-- nav bar -->
+      <div class="nav-menu">
+        <ul>
+          <li>all</li>
+          <li>active</li>
+          <li>completed</li>
+        </ul>
+      </div>
+
+      <!-- list em forma de tabela -->
+      <div>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Tarefa</th>
+      <th scope="col">Status</th>
+      <th scope="col">Data Início</th>
+      <th scope="col">Data Fim</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Marklak dlaksjdlakjs</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -11,8 +56,19 @@
 
 export default {
   name: 'HomeView',
+
   components: {
     // HelloWorld
+  },
+
+  methods: {
+    // item(e) {
+    //   console.log(e)
+    // }
   }
 }
 </script>
+
+<style>
+  @import '@/assets/css/home.css';
+</style>
